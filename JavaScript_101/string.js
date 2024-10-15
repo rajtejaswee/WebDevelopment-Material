42aa06226718642572496abf66943317ff1e214a
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-15 23:53:09 +0530

    Strings

---
+++ JavaScript_101/string.js
@@ -0,0 +1,14 @@
+const course = "JavaScript 101: coding for the beginners"
+console.log(course)
+
+//Method of the Strings
+console.log(course.length)
+console.log(course.search("coding"));
+console.log(course[0])
+console.log(course.slice(16, 22))
+console.log(course.substr(16, 6))
+console.log(course.toUpperCase())
+console.log(course.toLowerCase())
+
+words = course.split("");
+console.log(words)
