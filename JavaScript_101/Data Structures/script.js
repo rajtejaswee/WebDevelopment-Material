4186780dfd3bd8cf5250552b6171bb81861d489b
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-15 22:03:38 +0530

    Data Structures

---
+++ JavaScript_101/Data Structures/script.js
@@ -0,0 +1,13 @@
+//Data Stri=ucture
+const str = "A String";
+const num = 4;
+const flt = 3.14;
+const bool = true;
+const list = ["item 1", 4, 8.98, "Three", []];
+const thing = null; // memory is allocated but has nothing in it
+const notDefined = undefined; //currently holds no value
+const getAge = function () { age = 31; };
+const obj = {
+    'name': 'Raj',
+    'age' : '23'
+}
