43cedad3e39899ce4cdd96052ad42db483018c42
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-16 00:55:11 +0530

    Functions

---
+++ JavaScript_101/functions.js
@@ -0,0 +1,25 @@
+function addNumbers(param1, param2) {
+    return param1 + param2;
+}
+
+let total = addNumbers(90, 10)
+console.log(total)
+
+function greetings(name) {
+    return `Hello ${name}`
+}
+
+console.log(greetings("raj"))
+console.log(greetings("cat"))
+
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+}
+
+console.log(add("1", "9"))
+
+function multiply(a, b) {
+    return Number(a) * Number(b)
+}
+
+console.log(add("10", "9"))
