a22ac14bec3d0199b771b383603786c06efd9095
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-15 21:49:30 +0530

    Operators

---
+++ JavaScript_101/Operators/script.js
@@ -0,0 +1,10 @@
+// Operators are used to perform operations
+let a = 4, b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+console.log(a++);
+console.log(b--);
