48411aacc8cd2057a9526ef7cc327f16fec4f0e7
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-15 22:14:19 +0530

    Contional Statements

---
+++ JavaScript_101/Conditional_statement/script.js
@@ -0,0 +1,18 @@
+// Conditional Statements
+
+var name = "Raj";
+if (name == "Raj") {
+    console.log(name + " " + "is correct");
+} else {
+    console.log(name + " " + "is not correct");
+}
+
+const age = 23;
+if (age >= 18) {
+    console.log("He can vote");
+} else if (age >= 18 && age <= 23) {
+    console.log("He can drink the alcohol")
+}
+else {
+    console.log("Don't do anything");
+}
