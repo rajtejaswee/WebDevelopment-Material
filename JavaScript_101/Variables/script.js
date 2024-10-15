63ce635ff6721fa2ac3538bb65188ff60cd49121
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-15 21:43:57 +0530

    Variables

---
+++ JavaScript_101/Variables/script.js
@@ -0,0 +1,11 @@
+var name = "Hello World";
+console.log(name);
+
+//let and const Variables
+//let and const are block variables in javascript and var is the global variables
+
+let apples = 1; //can be changed as many times as we want to
+const weight = 56;
+
+console.log(apples);
+console.log(weight);
