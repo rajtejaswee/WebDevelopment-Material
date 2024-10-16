e7618ddd0a767c9e70e3424bb89ac9938869bf4b
Raj Tejaswee <rajtejaswee02@gmail.com>
2024-10-17 02:52:17 +0530

    Scope

---
+++ JavaScript_201/scope.js
@@ -0,0 +1,7 @@
+const num = 1234;
+
+if (num === 1234) {
+    const greeting = "Hello World";
+}
+
+// const num is accessible in if block whereas greeting isn't accessible outside the block, same is the condition is applied to let but if we use var we can access it outside the if block.
